6cc12bb48d96b12a469fdbdcdc241cfe9d69f933
jasyon-xu <jasyon7710@gmail.com>
2021-01-25 23:20:54 +0800

    使用MaterialApp类、设置主题颜色、字体风格

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello',
-        textDirection: TextDirection.ltr
+void main() => runApp(App());
+
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Learn'),
+          elevation: 30.0, //阴影效果
+        ),
+        body: Hello(),
       ),
-    )
-  );
+      theme: ThemeData(
+        primarySwatch: Colors.yellow //主题颜色
+      ),
+    );
+  }
+
+}
+
+class Hello extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello, Flutter',
+        textDirection: TextDirection.ltr,
+        // 设置字体风格
+        style: TextStyle(
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87,
+        ),
+      ),
+    );
+  }
 }
